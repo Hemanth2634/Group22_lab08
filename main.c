223c2c8631178a411a6fc83f5c422f2f6dc735ef
Aravind <arvichukli@gmail.com>
2024-10-31 20:09:22 +0530

    Initial

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
+
+
+#define RED_LED      (1U << 1)  // Port F Pin 1
+#define BLUE_LED     (1U << 2)  // Port F Pin 2
+#define GREEN_LED    (1U << 3)  // Port F Pin 3
+
+void initUART0(void) {
+       
+}
+
+
+int main(void) {
+   
+}
